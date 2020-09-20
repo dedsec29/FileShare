7dd55e1ecd64840de363373bb6ef9d24743a9807
Parth Pawar <parth290601@rediffmail.com>
2020-09-21 03:23:31 +0530

    Cleaned up routes, added logic to controllers

--- api/routes/repositories.js
+++ api/routes/repositories.js
@@ -1,116 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
 
 const checkAuth = require('../middleware/check-auth');
-
-const Repository = require('../models/repository'); //collection (mongo by default makes this to plural)
+const RepositoriesController = require('../controllers/repositories');
 
 router.get('/', (req, res, next)=> {
     res.status(200).json({message: 'Handling GET requests to /repositories'});
 });
 
 //creating new repo
-router.post('/:userID', checkAuth, (req, res, next)=> {
-    Repository.exists({userID: req.body.userID, repoName: req.body.repoName})
-    .then(data=> {
-        if (data) {
-            res.status(409).json({message: "This repository for the user already exists"});
-            return;
-        }
-        //Now data is null, we can safely create
-        let obj = JSON.parse(JSON.stringify(req.body));    //copying body elements
-
-        let repo = new Repository(obj);
-        //save this
-        repo.save() //.save() returns a true promise, unlike .find() etc so don't .exec()
-        .then(results=> { //sending appropriate status
-            console.log(results);
-            res.status(201).json({
-                message: "Handling POST requests to /repositories/"+req.params.userID,
-                createdRepository: results
-            });
-        })
-        .catch(err=> {
-            console.log(err);
-            res.status(500).json({error: err});
-        });
-    })
-    .catch(err=> {
-        console.log(err);
-        res.status(400).json({error: err});
-    });
-});
+router.post('/:userID', checkAuth, RepositoriesController.createRepo);
 
 //display all repositories
-router.get('/all', (req, res, next)=> {
-    Repository.find()
-    .then(results=> {res.send(results); console.log(results)})
-    .catch(err=> {
-        console.log(err);
-        res.status(500).json({error: err});
-    });
-});
+router.get('/all', RepositoriesController.displayRepos);
 
 //delete a repository
-router.delete('/:userID/:repoName', checkAuth, (req, res, next)=> {
-    let userID = req.params.userID;
-    let repoName = req.params.repoName;
-    Repository.deleteOne({userID:userID, repoName:repoName}).exec()
-    .then(results=> {
-        console.log(results);
-        if (results['deletedCount']!=0) {
-            res.status(200).json({
-                message: "Deleted Successfully",
-                result: results
-            });
-        }
-        else {
-            res.status(400).json({
-                message: "Couldn't find such repository or userID or combination of both",
-                result: results
-            });
-        }
-    })
-    .catch(err=> {
-        console.log(err);
-        res.status(500).json({error: err});
-    });
-});
+router.delete('/:userID/:repoName', checkAuth, RepositoriesController.deleteRepo);
 
 //update repository details 
-router.put('/:userID/:repoName', checkAuth, (req, res, next)=> {
-    let userID = req.params.userID;
-    let repoName = req.params.repoName;
-    let obj = JSON.parse(JSON.stringify(req.body));     //copying body elements (can additionally ensure that userID is not in body received)
-    Repository.updateOne({userID:userID, repoName:repoName}, {$set: obj}).exec()
-    .then(results=> {
-        console.log(results);
-        if (results['nModified']!=0) {
-            res.status(200).json({
-                message: "Update successful",
-                result: results
-            });
-        }
-        else {
-            if (results['n']!=0) {
-                res.status(400).json({
-                    message: "Couldn't find such repository", //if could not find userID, We will throw auth error for security reasons (in check-auth middleware)
-                    result: results
-                });
-            }
-            else { //Did not update because details were same as original
-                res.status(200).json({
-                    message: "Nothing to update",
-                    result: results
-                })
-            }
-        }
-    })
-    .catch(err=> {
-        console.log(err);
-        res.status(400).json({error:err});
-    });
-}); 
+router.put('/:userID/:repoName', checkAuth, RepositoriesController.updateRepo); 
 
 module.exports = router;
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,211 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 
 const checkAuth = require('../middleware/check-auth');
-
-const Repository = require('../models/repository');
-const User = require('../models/user'); //collection (mongo by default makes this to plural)
-
-const saltAmount = 10;
+const UsersController = require('../controllers/users');
 
 router.get('/', (req, res, next)=> {
     res.status(200).json({message: 'Handling GET requests to /users'});
 });
 
 //create new user
-router.post('/signup', (req, res, next)=> {
-    if (!req.body.hasOwnProperty('email') || !req.body.hasOwnProperty('userID') || !req.body.hasOwnProperty('password')) {
-        return res.status(400).json({message: "Missing required details"});
-    }
-    User.exists({userID: req.body.userID})
-    .then(data1=> {
-        if (data1) {
-            return res.status(409).json({message: "User ID taken"});
-        }
-        User.exists({email: req.body.email})
-        .then(data2=> {
-            if (data2) {
-                return res.status(409).json({message: "Email ID taken"});
-            }
-            let obj = JSON.parse(JSON.stringify(req.body));    //copying body elements
-            //hash the password
-            bcrypt.hash(obj['password'], saltAmount)
-            .then(hash=> {
-                    obj['password'] = hash;
-                    obj['email'] = obj['email'].toLowerCase();
-                    let userObj = new User(obj);
-                    return userObj.save()
-            }) 
-            .then(results=> {
-                console.log(results);
-                res.status(201).json({message: "User created"});
-            })   
-            .catch(err=> {
-                console.log(err);
-                res.status(500).json({error: err});
-            });
-        })
-        .catch(err=> {
-            console.log(err);
-            res.status(500).json({error: err});
-        });
-    })
-    .catch(err=> {
-        console.log(err);
-        res.status(500).json({error: err});
-    });
-});
+router.post('/signup', UsersController.createUser);
 
 /*  Display all users (For future: Remember to not display password of users.
     Also display selective details if user 'Access' is private) */
-router.get('/all', (req, res, next)=> {
-    User.find()
-    .then(results=> {res.send(results); console.log(results)})
-    .catch(err=> {
-        console.log(err);
-        res.status(500).json({error: err});
-    });
-});
+router.get('/all', UsersController.displayUsers);
 
 //delete a user, also all its repositories too
-router.delete('/:userID', checkAuth,(req, res, next)=> {
-    let userID = req.params.userID;
-    User.deleteOne({userID: userID}).exec()
-    .then(results1=> {
-        if (results1['deletedCount']==0) {
-            console.log(results1);
-            res.status(400).json({
-                message: "User does not exist, cannot delete",
-                result: results1
-            });
-        }
-        else {
-            Repository.deleteMany({userID: userID}).exec()
-            .then(results2=> {
-                console.log(results2);
-                res.status(200).json({message: "deletion successful",});
-            })
-            .catch(err=> {
-                console.log(err);
-                res.status(500).json({error: err});
-            });
-        }
-    })
-    .catch(err=> {
-        console.log(err);
-        res.status(500).json({error: err});
-    });
-});
+router.delete('/:userID', checkAuth, UsersController.deleteUser);
 
 //update user details. If userID itself is changed, reflect the changes on Repository model as well
-router.put('/:userID', checkAuth, async (req, res, next)=> {
-    let userID = req.params.userID;
-    let obj = JSON.parse(JSON.stringify(req.body));    //copying body elements
-    if (obj.hasOwnProperty('password')) { //password needed to be updated needs to be hashed too
-        try {
-            obj['password'] = await bcrypt.hash(obj['password'], saltAmount);
-        }
-        catch(err) {
-            console.log(err);
-            return res.status(500).json({error: err});
-        }
-    }
-    User.updateOne({userID: userID}, {$set: obj}).exec()
-    .then(results1=> {
-        if (results1['nModified']==0) {
-            console.log(results1)
-            if (results1['n']==0) {
-                res.status(400).json({
-                    message: "User does not exist, cannot update"
-                });
-            }
-            else {  //else it means that user exists, but details were same as original
-                res.status(200).json({
-                    message: "Nothing to update"
-                });
-            }
-        }
-        else {
-            //user details updated. If userID updated, reflect changes in other models
-            if (obj.hasOwnProperty('userID') && userID!=obj['userID']) {
-                Repository.updateMany({userID: userID}, {$set: {userID: obj['userID']} }).exec()
-                .then(results2=> {
-                    console.log(results2);
-                    res.status(200).json({
-                        message: "Updates Applied"
-                    }); 
-                })
-                .catch(err=> {
-                    console.log(err);
-                    res.status(500).json({error: err});
-                });
-            }
-            else {
-                console.log(results1);
-                res.status(200).json({
-                    message: "Updates Applied"
-                }); 
-            }
-        }
-    })
-    .catch((err)=> {
-        console.log(err);
-        res.status(500).json({error: err});
-    });
-});
+router.put('/:userID', checkAuth, UsersController.updateUser);
 
-router.post('/login', (req, res, next)=> {
-    //User can log in using either email or userID
-    let qry = {};
-    if (req.body.hasOwnProperty('userID'))
-        qry['userID'] = req.body.userID;
-    else if (req.body.hasOwnProperty('email'))
-        qry['email'] = req.body.email;
-    else 
-        return res.status(400).json({message: "Email or UserID was not provided"});
-
-    if (req.body.hasOwnProperty('password')==false)
-        return res.status(400).json({message: "Password was not provided"});
-    
-    User.find(qry).exec()
-    .then(user=> {
-        if (user.length < 1) {
-            return res.status(401).json({message: "Authorization failed"}); //do not indicate that user doesn't exist for security reasons
-        }
-        bcrypt.compare(req.body.password, user[0].password)
-        .then(result=> {
-            if (result) {
-                const token = jwt.sign(
-                    {   //payload of token
-                        userID: user[0].userID,
-                        email: user[0].email
-                    },
-                    process.env.JWT_KEY,
-                    {
-                        expiresIn: "1h"
-                    }
-                );
-
-                return res.status(200).json({
-                    message: "Authorization successful",
-                    token: token
-                });
-            }
-            else {
-                return res.status(401).json({message: "Authorization failed"});
-            }
-        })
-        .catch(err=> {
-            res.status(401).json({message: "Authorization failed"});
-        });
-    })
-    .catch(err=> {
-        console.log(err);
-        res.status(500).json({error: err});
-    });
-})
+router.post('/login', UsersController.login);
 
 module.exports = router;
