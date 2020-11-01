99754702199325a2a41ef3608fd64f245fb35d59
Parth Pawar <parth290601@rediffmail.com>
2020-11-01 12:55:30 +0530

    Fixing small issues

--- api/middleware/multer.js
+++ api/middleware/multer.js
@@ -1,7 +1,7 @@
 const multer = require('multer');
 const path = require('path');
 const storage = multer.diskStorage({
-  destination: '../uploads',
+  destination: './uploads',
   filename: (req, file, cb) => {
     cb(
       null,
--- api/routes/uploads.js
+++ api/routes/uploads.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const storedPicture = require('../controllers/uploads');
 const { upload } = require('../middleware/multer');
-router.post('/upload', upload.single('picture'), storedPicture);
+router.post('/uploads', upload.single('picture'), storedPicture);
 
 module.exports = router;
